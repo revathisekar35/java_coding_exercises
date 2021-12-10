eeaf754ba922961a5f9dbff6c388e736cfc0e9dc
revathisekar35 <revathisekar2010@gmail.com>
2021-12-10 00:56:02 +0000

    Implemented Exercise002 - creation of object

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,15 +1,31 @@
 package com.techreturners.exercise002;
 
 public class Exercise002 {
+	/**
+	 * This method is to check the person from "Manchester" city or not
+	 * 
+	 * @param person
+	 * @return boolean
+	 */
+	public boolean isFromManchester(Person person) {
+		if (person != null && person.getCity().equalsIgnoreCase("Manchester")) {
+			return true;
+		}
+		return false;
+	}
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
-    }
+	/**
+	 * This methods determines whether the person is allowed to watch film or not
+	 * 
+	 * @param person
+	 * @param ageLimit
+	 * @return boolean
+	 */
+	public boolean canWatchFilm(Person person, int ageLimit) {
+		if (person != null && person.getAge() == 18) {
+			return true;
+		}
+		return false;
+	}
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
-    }
-    
 }
